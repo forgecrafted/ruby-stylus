ccb522e3754370b1507bd9fc370bd7e2f31e59fa
Lucas Mazza <luc4smazza@gmail.com>
2011-06-05 20:43:11 -0300

    workaround to register Stylus on production environments.

--- lib/stylus/railtie.rb
+++ lib/stylus/railtie.rb
@@ -5,11 +5,15 @@ module Stylus
 
     initializer :setup_stylus do |app|
       config.app_generators.stylesheet_engine :styl
+      next unless app.config.assets.enabled
+
+      require 'sprockets'
+      require 'sprockets/engines'
+      Sprockets.register_engine '.styl', Tilt::StylusTemplate
     end
 
     config.after_initialize do |app|
       Stylus.paths.concat app.assets.paths
-      app.assets.register_engine '.styl', Tilt::StylusTemplate
     end
 
   end
