12c263d7f25552e3f07dd8a9093cadb359eb4921
Lucas Mazza <luc4smazza@gmail.com>
2011-07-14 21:15:33 -0300

    copy'n paste is so wrong

--- lib/rails/generators/stylus/assets/assets_generator.rb
+++ lib/rails/generators/stylus/assets/assets_generator.rb
@@ -5,7 +5,7 @@ module Stylus
     class AssetsGenerator < ::Rails::Generators::NamedBase
       source_root File.expand_path("../templates", __FILE__)
 
-      def copy_coffee
+      def copy_stylus
         template "stylesheet.css.styl", File.join('app/assets/stylesheets', class_path, "#{file_name}.css.styl")
       end
     end
