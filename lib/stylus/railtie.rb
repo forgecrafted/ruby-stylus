a25bafa82f300787bf91d5bd9b5f8d12b6d7cc31
Alex MacCaw <maccman@gmail.com>
2011-08-26 21:15:04 +0100

    fix paths

--- lib/stylus/railtie.rb
+++ lib/stylus/railtie.rb
@@ -7,14 +7,16 @@ module Stylus
   class Railtie < ::Rails::Railtie
 
     config.app_generators.stylesheet_engine :stylus
-    
+
+    initializer :setup_stylus do |app|
+      app.assets.register_engine '.styl', Tilt::StylusTemplate
+    end
+
     # Includes the `Rails` asset load path into `stylus` so any
     # `.styl` file inside it can be imported by the `stylus` API.
-    initializer "stylus.initialize", :after => "sprockets.environment" do |app|
+    config.after_initialize do |app|
       Stylus.compress = app.config.assets.compress
       Stylus.paths.concat app.assets.paths
-
-      app.assets.register_engine '.styl', Tilt::StylusTemplate      
     end
   end
 end
