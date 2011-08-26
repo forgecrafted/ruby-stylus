9ef5add8f01b62de7dda5af9d24ea474f17b7ac4
Alex MacCaw <maccman@gmail.com>
2011-08-26 20:54:42 +0100

    init after sprockets

--- lib/stylus/railtie.rb
+++ lib/stylus/railtie.rb
@@ -7,16 +7,14 @@ module Stylus
   class Railtie < ::Rails::Railtie
 
     config.app_generators.stylesheet_engine :stylus
-
-    initializer :setup_stylus do |app|
-      app.assets.register_engine '.styl', Tilt::StylusTemplate
-    end
-
+    
     # Includes the `Rails` asset load path into `stylus` so any
     # `.styl` file inside it can be imported by the `stylus` API.
-    config.after_initialize do |app|
+    initializer "stylus.initialize", :after => "sprockets.environment" do |app|
       Stylus.compress = config.assets.compress
       Stylus.paths.concat app.assets.paths
+
+      app.assets.register_engine '.styl', Tilt::StylusTemplate      
     end
   end
 end
