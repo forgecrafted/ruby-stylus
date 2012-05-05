5a202551c545415881eaf6669722351892354b4f
Lucas Mazza <luc4smazza@gmail.com>
2012-05-05 00:08:37 -0300

    wtf is going on with this old code

--- spec/import_processor_spec.rb
+++ spec/import_processor_spec.rb
@@ -1,20 +1,13 @@
 require 'spec_helper'
 
 describe Stylus::ImportProcessor do
-
-  let(:app) { create_app }
-  let(:env) { app.assets }
-  let(:sprockets) { double(:environment => env) }
-
   it 'adds an imported stylesheet as a dependency' do
-    source = fixture(:import).first
-    asset = env['import']
-    template = Stylus::ImportProcessor.new { source }
+    source     = fixture(:import).first
+    template   = Stylus::ImportProcessor.new { source }
     dependency = Pathname.new(fixture_path('mixins/vendor'))
-    sprockets.stub(:resolve) { dependency }
+    sprockets  = double(:resolve => dependency)
 
     sprockets.should_receive(:depend_on).with(dependency)
     template.render(sprockets)
   end
-
 end
