870d1282a75596ef3992c6237151bbb0e041dcdc
Lucas Mazza <luc4smazza@gmail.com>
2012-05-04 23:37:20 -0300

    Don't reference the asset directly on the ImportProcessor, fixes #24.
    Doing this triggers the compilation of stylesheets during this step,
    and we shouldn't do so.

--- spec/import_processor_spec.rb
+++ spec/import_processor_spec.rb
@@ -4,16 +4,17 @@ describe Stylus::ImportProcessor do
 
   let(:app) { create_app }
   let(:env) { app.assets }
-  let(:context) { double(:environment => env) }
+  let(:sprockets) { double(:environment => env) }
 
   it 'adds an imported stylesheet as a dependency' do
     source = fixture(:import).first
     asset = env['import']
     template = Stylus::ImportProcessor.new { source }
     dependency = Pathname.new(fixture_path('mixins/vendor'))
+    sprockets.stub(:resolve) { dependency }
 
-    context.should_receive(:depend_on_asset).with(dependency)
-    template.render(context)
+    sprockets.should_receive(:depend_on).with(dependency)
+    template.render(sprockets)
   end
 
 end
